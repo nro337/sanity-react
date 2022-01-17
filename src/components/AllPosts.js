62b6b25047c594bfc44e63349a9e17b2fd6af310
nro337 <nro337@gmail.com>
2022-01-16 19:42:50 -0500

    DaisyUI, Homepage styling

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -3,6 +3,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+import logo from "../assets/thekick.png";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function AllPosts() {
   const [allPostsData, setAllPosts] = useState(null);
@@ -18,7 +25,10 @@ export default function AllPosts() {
           _id,
           url
         }
-      }
+      },
+      body,
+      "name": author->name,
+      "authorImage": author->image
     }`
       )
       .then((data) => setAllPosts(data))
@@ -26,12 +36,101 @@ export default function AllPosts() {
   }, []);
 
   return (
-    <div className="bg-green-100 min-h-screen p-12">
+    <div className="bg-slate-100 min-h-screen p-12">
+      <div class="navbar mb-6 shadow-lg bg-neutral text-neutral-content rounded-box">
+        <div class="flex-1 px-2 mx-2">
+          <img src={logo} alt="" width={80} className="rounded-xl" />
+        </div>
+        <div class="flex-none hidden px-2 mx-2 lg:flex">
+          <div class="flex items-stretch">
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-5 mr-2 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              Likes
+            </a>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-5 mr-2 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                ></path>
+              </svg>
+              Notifications
+            </a>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-5 mr-2 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                ></path>
+              </svg>
+              Files
+            </a>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-5 mr-2 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                ></path>
+              </svg>
+              Config
+            </a>
+          </div>
+        </div>
+        <div class="flex-none">
+          <button class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
       <div className="container mx-auto">
-        <h2 className="text-5xl flex justify-center cursive">Blog Posts</h2>
-        <h3 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my blog posts page!
-        </h3>
+        <h2 className="text-6xl flex justify-center cursive text-blue-400 mb-8">
+          The Lead
+        </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {allPostsData &&
             allPostsData.map((post, index) => (
@@ -43,16 +142,17 @@ export default function AllPosts() {
                 >
                   <img
                     className="w-full h-full rounded-r object-cover absolute"
-                    src={post.mainImage.asset.url}
+                    src={urlFor(post.mainImage).url()}
                     alt=""
                   />
                   <span
-                    className="block relative h-full flex justify-end items-end pr
+                    className="block relative h-full flex justify-between items-end pr
                       -4 pb-4"
                   >
+                    <img src={urlFor(post.authorImage).url()} alt="" className="rounded-full w-16 h-16 ml-2 border-2 object-cover shadow-xl"/>
                     <h2
-                      className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700
-                        text-red-100 bg-opacity-75 rounded"
+                      className="text-gray-800 text-lg font-bold px-3 py-3 bg-gray-900
+                        text-red-100 bg-opacity-75 rounded-l"
                     >
                       {post.title}
                     </h2>
