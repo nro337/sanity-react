3bf1531c8ec1c2cc70cf61d51d52d70a8257a318
nro337 <nro337@gmail.com>
2022-01-30 18:23:01 -0500

    Responsive navbar handling, padding cleanup

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -36,198 +36,163 @@ export default function AllPosts() {
   }, []);
 
   return (
-    <div className="bg-slate-100 min-h-screen p-12">
-      <div class="navbar mb-6 shadow-lg bg-neutral text-neutral-content rounded-box">
-        <div class="flex-1 px-2 mx-2">
-          <a href="/">
-            <img src={logo} alt="" width={80} className="rounded-xl" />
-          </a>
-        </div>
-        <div class="flex-none hidden px-2 mx-2 lg:flex">
-          <div class="flex items-stretch">
-            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-              Likes
-            </a>
-            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                ></path>
-              </svg>
-              Notifications
-            </a>
-            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                ></path>
-              </svg>
-              Files
-            </a>
-            <a class="btn btn-ghost btn-sm rounded-btn" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-                ></path>
-              </svg>
-              Config
-            </a>
+    <div className="bg-slate-100">
+      <div class="shadow bg-base-200 drawer h-full">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="flex flex-col drawer-content">
+          <div class="w-full navbar bg-base-300">
+            <div class="flex-none lg:hidden">
+              <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  class="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div class="pr-4 mx-auto lg:ml-40">
+              <a href="/">
+                <img src={logo} alt="" width={80} className="rounded-xl" />
+              </a>
+            </div>
+            <div class="flex-none hidden lg:block">
+              <ul class="menu horizontal">
+                <li>
+                  <a class="rounded-btn" href="/">
+                    Item 1
+                  </a>
+                </li>
+                <li>
+                  <a class="rounded-btn" href="/">
+                    Item 2
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-      <div className="container mx-auto">
-        <h2 className="text-6xl flex justify-center cursive text-blue-400 mb-8">
-          The Lead
-        </h2>
-        <div className="grid md:grid-cols-3 lg:grid-cols-5 grid-rows-1 gap-8">
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:col-span-3 lg:col-span-4">
-            {allPostsData &&
-              allPostsData.map((post, index) => (
-                <Link to={"/" + post.slug.current} key={post.slug.current}>
-                  <span
-                    className="block h-64 w-full relative rounded shadow leading-snug bg-white indicator"
-                    key={index}
-                  >
-                    <div className="indicator-item badge">Comments: 99+</div>
-                    <img
-                      className="w-full h-full rounded object-cover absolute"
-                      src={urlFor(post.mainImage).url()}
-                      alt=""
-                    />
-                    <span
-                      className="block relative h-full flex justify-between items-end pr
-                      -4 pb-4"
-                    >
-                      <img
-                        src={urlFor(post.authorImage).url()}
-                        alt=""
-                        className="mask mask-diamond w-20 h-20 ml-2 object-cover shadow-xl"
-                      />
-                      <h2
-                        className="text-gray-800 text-lg font-bold px-3 py-3 bg-gray-900
-                        text-red-100 bg-opacity-75 rounded-l"
+          <div className="container mx-auto pb-8">
+            <h2 className="text-6xl flex justify-center cursive text-blue-400 my-4">
+              The Lead
+            </h2>
+            <div className="grid px-4 md:grid-cols-3 lg:grid-cols-5 grid-rows-1 gap-8">
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:col-span-3 lg:col-span-4">
+                {allPostsData &&
+                  allPostsData.map((post, index) => (
+                    <Link to={"/" + post.slug.current} key={post.slug.current}>
+                      <span
+                        className="block h-64 w-full relative rounded shadow leading-snug bg-white indicator"
+                        key={index}
                       >
-                        {post.title}
-                      </h2>
-                    </span>
-                  </span>
-                </Link>
-              ))}
+                        <div className="indicator-item badge badge-accent">
+                          99+
+                        </div>
+                        <img
+                          className="w-full h-full rounded object-cover absolute"
+                          src={urlFor(post.mainImage).url()}
+                          alt=""
+                        />
+                        <span
+                          className="block relative h-full flex justify-between items-end pr
+                      -4 pb-4"
+                        >
+                          <img
+                            src={urlFor(post.authorImage).url()}
+                            alt=""
+                            className="mask mask-diamond w-20 h-20 ml-2 object-cover shadow-xl"
+                          />
+                          <h2
+                            className="text-gray-800 text-lg font-bold px-3 py-3 bg-gray-900
+                        text-red-100 bg-opacity-75 rounded-l"
+                          >
+                            {post.title}
+                          </h2>
+                        </span>
+                      </span>
+                    </Link>
+                  ))}
+              </div>
+              <div className="col-span-1 grid-row-1 w-auto min-h-screen">
+                <h1 className="text-center mb-2">Recent Meets</h1>
+                <iframe
+                  title="TFRRS"
+                  src="//rss.bloople.net/?url=https%3A%2F%2Fwww.tfrrs.org%2Fresults.rss&showicon=true&type=html"
+                  className="rounded bg-blue-400 text-red-500 w-full h-full"
+                ></iframe>
+              </div>
+            </div>
           </div>
-          <div className="col-span-1 grid-row-1 w-auto min-h-screen">
-            <iframe
-              title="TFRRS"
-              src="//rss.bloople.net/?url=https%3A%2F%2Fwww.tfrrs.org%2Fresults.rss&showicon=true&type=html"
-              className="rounded bg-blue-400 text-red-500 w-full h-full"
-            ></iframe>
-          </div>
+          <footer class="items-center p-4 mt-8 footer bg-neutral text-neutral-content rounded-box bg-base-300">
+            <div class="items-center grid-flow-col">
+              <svg
+                width="36"
+                height="36"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                class="fill-current"
+              >
+                <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+              </svg>
+              <p>The Lead - 2022</p>
+            </div>
+            <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+              <a href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="fill-current"
+                >
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                </svg>
+              </a>
+              <a href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="fill-current"
+                >
+                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+              </a>
+              <a href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="fill-current"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
+            </div>
+          </footer>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-3" class="drawer-overlay"></label>
+          <ul class="p-4 overflow-y-auto menu w-80 bg-base-100">
+            <li>
+              <a href="/">Item 1</a>
+            </li>
+            <li>
+              <a href="/">Item 2</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <footer class="items-center p-4 mt-8 footer bg-neutral text-neutral-content rounded-box">
-        <div class="items-center grid-flow-col">
-          <svg
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            class="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>The Lead - 2022</p>
-        </div>
-        <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-          <a href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
-            >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-            </svg>
-          </a>
-          <a href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
-            >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
-          </a>
-          <a href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </a>
-        </div>
-      </footer>
     </div>
   );
 }
